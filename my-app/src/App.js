95bb3ed4ea9860d12b42e154fd4eda622eeee4f8
Muhammad Saif Ur Rehman <120719748+saifee403@users.noreply.github.com>
2023-05-18 14:30:10 +0500

    adding some css

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,32 +2,43 @@ import './App.css';
 import Subscriptions from './components/Subscriptions';
 
 function App() {
-  let subscriptions = [
+  let subscription=[
     {
       id:"1",
-      date : (new Date("2023","05","18")),
-      title : "Monthly Subscriptions",
-      amount : "250"
+      date:(new Date("2023","05","18")),
+      title:"Monthly Sub",
+      price: "15$"
     },
     {
       id:"2",
-      date : (new Date("2023","05","18")),
-      title : "Yearly Subscriptions",
-      amount : "250"
+      date:(new Date("2023","05","18")),
+      title:"Weekly Sub",
+      price: "4$"
     },
     {
       id:"3",
-      date : (new Date("2023","05","18")),
-      title : "weekly Subscriptions",
-      amount : "250"
+      date:(new Date("2023","05","18")),
+      title:"Yearly Sub",
+      price: "150$"
+    },
+    {
+      id:"4",
+      date:(new Date("2023","05","18")),
+      title:"Daily Sub",
+      price: "0.20$"
     }
   ]
+  
   return (
     <div className="App">
       <h1>Muhammad Saif Ur Rehman</h1>
-      <Subscriptions passDate={subscriptions[0].date.toISOString()} passTitle={subscriptions[0].title} passAmount={subscriptions[0].amount}/>
+      <Subscriptions date={subscription[0].date.toISOString()} title={subscription[0].title} amount={subscription[0].price}/>
+      <Subscriptions date={subscription[1].date.toISOString()} title={subscription[1].title} amount={subscription[1].price}/>
+      <Subscriptions date={subscription[1].date.toISOString()} title={subscription[2].title} amount={subscription[2].price}/>
+
+      {/* <Subscriptions passDate={subscriptions[0].date.toISOString()} passTitle={subscriptions[0].title} passAmount={subscriptions[0].amount}/>
       <Subscriptions passDate={subscriptions[1].date.toISOString()} passTitle={subscriptions[1].title} passAmount={subscriptions[1].amount}/>
-      <Subscriptions passDate={subscriptions[2].date.toISOString()} passTitle={subscriptions[2].title} passAmount={subscriptions[2].amount}/>
+      <Subscriptions passDate={subscriptions[2].date.toISOString()} passTitle={subscriptions[2].title} passAmount={subscriptions[2].amount}/> */}
     </div>
   );
 }
--- my-app/src/components/Subscriptions.jsx
+++ my-app/src/components/Subscriptions.jsx
@@ -1,16 +1,35 @@
 import React from 'react'
+import "./Subscription.css"
 
 function Subscriptions(props) {
-    // let date = (new Date("2023","05","18"))
-    // let title = "Monthly Subscriptions"
-    // let amount = "250"
   return (
-    <div>
-      <h1>{props.passDate}</h1>
-      <h2>{props.passTitle}</h2>
-      <h3>{props.passAmount}</h3>
+    <div className='subscription'>
+        <div className="time">{props.date}</div>
+        <div className="title">{props.title}</div>
+        <div className="amount">{props.amount}</div>
+      
     </div>
   )
 }
 
 export default Subscriptions
+
+
+
+// import React from 'react'
+// import "./Subscription.css"
+
+// function Subscriptions(props) {
+//     // let date = (new Date("2023","05","18"))
+//     // let title = "Monthly Subscriptions"
+//     // let amount = "250"
+//   return (
+//     <div className='subscription'>
+//       <h1>{props.passDate}</h1>
+//       <h2>{props.passTitle}</h2>
+//       <h3>{props.passAmount}</h3>
+//     </div>
+//   )
+// }
+
+// export default Subscriptions
