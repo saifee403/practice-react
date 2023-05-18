d1f4ef051908f200daeac13538fb4192dab19afc
Muhammad Saif Ur Rehman <120719748+saifee403@users.noreply.github.com>
2023-05-18 14:06:16 +0500

    adding props

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,10 +2,32 @@ import './App.css';
 import Subscriptions from './components/Subscriptions';
 
 function App() {
+  let subscriptions = [
+    {
+      id:"1",
+      date : (new Date("2023","05","18")),
+      title : "Monthly Subscriptions",
+      amount : "250"
+    },
+    {
+      id:"2",
+      date : (new Date("2023","05","18")),
+      title : "Yearly Subscriptions",
+      amount : "250"
+    },
+    {
+      id:"3",
+      date : (new Date("2023","05","18")),
+      title : "weekly Subscriptions",
+      amount : "250"
+    }
+  ]
   return (
     <div className="App">
       <h1>Muhammad Saif Ur Rehman</h1>
-      <Subscriptions/>
+      <Subscriptions passDate={subscriptions[0].date.toISOString()} passTitle={subscriptions[0].title} passAmount={subscriptions[0].amount}/>
+      <Subscriptions passDate={subscriptions[1].date.toISOString()} passTitle={subscriptions[1].title} passAmount={subscriptions[1].amount}/>
+      <Subscriptions passDate={subscriptions[2].date.toISOString()} passTitle={subscriptions[2].title} passAmount={subscriptions[2].amount}/>
     </div>
   );
 }
--- my-app/src/components/Subscriptions.jsx
+++ my-app/src/components/Subscriptions.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 
-function Subscriptions() {
-    let date = (new Date("2023","05","18"))
-    let title = "Monthly Subscriptions"
-    let amount = "250"
+function Subscriptions(props) {
+    // let date = (new Date("2023","05","18"))
+    // let title = "Monthly Subscriptions"
+    // let amount = "250"
   return (
     <div>
-      <h1>{date.toISOString()}</h1>
-      <h2>{title}</h2>
-      <h3>{amount}</h3>
+      <h1>{props.passDate}</h1>
+      <h2>{props.passTitle}</h2>
+      <h3>{props.passAmount}</h3>
     </div>
   )
 }
