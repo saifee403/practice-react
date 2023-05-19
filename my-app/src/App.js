523d0632f294d4dd085bf7fdb7823a6c209fe2e2
Muhammad Saif Ur Rehman <120719748+saifee403@users.noreply.github.com>
2023-05-19 14:30:38 +0500

    app js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -41,11 +41,11 @@ function App() {
     <NAvbar/>
     <div className="App">
       <h1>Muhammad Saif Ur Rehman</h1>
-      <Subscriptions date={subscription[0].date.toISOString()} title={subscription[0].title} amount={subscription[0].price}/>
-      <Subscriptions date={subscription[1].date.toISOString()} title={subscription[1].title} amount={subscription[1].price}/>
-      <Subscriptions date={subscription[2].date.toISOString()} title={subscription[2].title} amount={subscription[2].price}/> 
-      <Subscriptions date={subscription[3].date.toISOString()} title={subscription[3].title} amount={subscription[3].price}/> 
-      <Subscriptions date={subscription[4].date.toISOString()} title={subscription[4].title} amount={subscription[4].price}/> 
+      <Subscriptions date={subscription[0].date} title={subscription[0].title} amount={subscription[0].price}/>
+      <Subscriptions date={subscription[1].date} title={subscription[1].title} amount={subscription[1].price}/>
+      <Subscriptions date={subscription[2].date} title={subscription[2].title} amount={subscription[2].price}/> 
+      <Subscriptions date={subscription[3].date} title={subscription[3].title} amount={subscription[3].price}/> 
+      <Subscriptions date={subscription[4].date} title={subscription[4].title} amount={subscription[4].price}/> 
     </div>
     </>
   );
--- my-app/src/components/Subscriptions.jsx
+++ my-app/src/components/Subscriptions.jsx
@@ -2,11 +2,16 @@ import React from 'react'
 import "./Subscription.css"
 
 function Subscriptions(props) {
+  const month=props.date.toLocaleString('default',{month:'long'})
+  const day=props.date.toLocaleString('default',{day:'2-digit'})
+  const year=props.date.getFullYear()
   return (
     <>
     <div className='subscription'>
-        <div className="subscription_title">{props.date}</div>
-        <h2><div className="title">{props.title}</div></h2>
+        <div>{month}</div>
+        <div>{day}</div>
+        <div>{year}</div>
+        <h2 className="subscription_title">{props.title}</h2>
         <div className="subscription_price ">{props.amount}</div>
       
     </div>
