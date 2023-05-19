d3cf5c695e7774a23a71597f60fe397b0f61edcb
Muhammad Saif Ur Rehman <120719748+saifee403@users.noreply.github.com>
2023-05-19 15:59:25 +0500

    new css

--- my-app/src/components/Subscriptions.jsx
+++ my-app/src/components/Subscriptions.jsx
@@ -1,16 +1,12 @@
 import React from 'react'
 import "./Subscription.css"
+import SubscriptionDate from './SubscriptionDate'
 
 function Subscriptions(props) {
-  const month=props.date.toLocaleString('default',{month:'long'})
-  const day=props.date.toLocaleString('default',{day:'2-digit'})
-  const year=props.date.getFullYear()
   return (
     <>
     <div className='subscription'>
-        <div>{month}</div>
-        <div>{day}</div>
-        <div>{year}</div>
+      <SubscriptionDate date={props.date}/>
         <h2 className="subscription_title">{props.title}</h2>
         <div className="subscription_price ">{props.amount}</div>
       
